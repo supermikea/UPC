89fbb6ca83053430216d24f3ae52a0c257baf261
Mike Wiegman Avila <mike12wiegman@gmail.com>
2022-09-04 11:51:17 +0200

    preparation

---
+++ main/main_python_file.py
@@ -0,0 +1,8 @@
+import sys
+
+choice = sys.argv[1]
+if choice == 'pythagoras':
+    print('choice equals pythagoras')
+else:
+    print('usage: python main_python_file.py {choice} {args}')
+print('this is the main python file!')
